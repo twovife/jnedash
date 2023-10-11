74a5307f5bc53887f5e80d76a562f7144d3da7bc
twovife <azizihsan666@gmail.com>
2023-10-11 15:44:34 +0700

    commit

--- public/build/assets/Layout.2061cc1c.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as n,a}from"./app.d74aa348.js";const o=({header:s,children:t,...i})=>e(n,{children:[e("div",{className:"p-6 flex justify-between mx-auto max-w-5xl",children:[a("h1",{className:"text-xl font-semibold text-indigo-600",children:"Pusat Bantuan"}),a("div",{className:"font-semibold",children:s})]}),a("main",{children:t})]});export{o as default};
